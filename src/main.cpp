83e6ed0e1ad86951916496180dd48aacbede6acd
Westley Davis <wes@westleydavis.com>
2017-12-03 02:09:58 -0800

    Partial Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,10 @@
-//#include "StandardCplusplus.h"
-
 #include "main.hpp"
-// uint8_t DriveMotor_State = DriveMotor_Stop;
-enum motor_type {
-    DriveMotor_Stop,
-    DriveMotor_MovingLeft,
-    DriveMotor_MovingRight,
-    DriveMotor_PausedLeft,
-    DriveMotor_PausedRight,
-    DriveMotor_Error,
-    DriveMotor_Engaged
-};
-motor_type DriveMotor_State = DriveMotor_Stop;
-enum display_type {
-    Display_MotorSpeed,
-    Display_Min,
-    Display_ShakeMotor_On,
-    Display_ShakeMotor_Off,
-    Display_Stop,
-    Display_Error,
-    Display_Sensor_Test
-};
-display_type Display_State;// = Display_Stop;
 
+
+display_type Display_State;
 Adafruit_DCMotor *DriveMotor;
 Adafruit_DCMotor *ShakeMotor;
 Adafruit_MotorShield AFMotorShield;
-/*/////////////////////////
-//
-//
-// work on time and states
-//
-//
-*/////////////////////////
-//moved back to blank space
-//using namespace std;
-//using namespace std;
 
 
 int counter=0;
@@ -52,39 +21,27 @@ unsigned long DisplayTime = 0;
 unsigned long GoTime = 0;
 bool Time_Transition = false;
 
-//end moved back to blank space
+
 
 void Error();
 void MovingLeft(bool LeftSensor, bool RightSensor);
 void MovingRight(bool LeftSensor, bool RightSensor);
 void PausedLeft(bool LeftSensor,bool RightSensor);
 void PausedRight(bool LeftSensor, bool RightSensor);
+void shakemotor();
 void Stop();
 void Sensor_Test();
 
 void setup() {
-    //AFMotorShield = Adafruit_MotorShield();
-
     DriveMotor = AFMotorShield.getMotor(3);
     ShakeMotor = AFMotorShield.getMotor(4);
     AFMotorShield.begin();
-    DriveMotor->setSpeed(255); //(150)
-    //DriveMotor->run(FORWARD);
+    DriveMotor->setSpeed(255);
     ShakeMotor->setSpeed(75);
-    //ShakeMotor->run(FORWARD);
     Timer1.initialize();
     MFS.initialize(&Timer1);    // initialize multi-function shield library
-/*
-    MFS.write("Hi");
-    delay(2000);
-    MFS.write(-273);
-    delay(2000);
-    MFS.write(3.141, 3);  // display to 2 decimal places.
-    delay(2000);
-    */
     pinMode(LeftSensorPin, INPUT);
     pinMode(RightSensorPin, INPUT);
-    //*
     delay(500);
     byte btn = MFS.getButton();
     delay(500);
@@ -92,95 +49,52 @@ void setup() {
     if((btn==BUTTON_1_PRESSED) | (btn==BUTTON_2_PRESSED) | (btn==BUTTON_3_PRESSED)){
         Display_State = Display_Sensor_Test;
     }
-    if(btn==BUTTON_1_LONG_PRESSED){
-        //Display_State = Display_Sensor_Test;
-    }
-    //*/
-
-
 }
-
-
-
 void loop() {
-    if(ShakeMotorRunning){
-        ShakeMotor->run(FORWARD);
-    }
-    else{
-        ShakeMotor->run(RELEASE);
-    }
-
-    byte btn = MFS.getButton();
+  //get sensors
+  byte btn = MFS.getButton();
   LeftSensor = !digitalRead(LeftSensorPin);
   RightSensor = !digitalRead(RightSensorPin);
-  //*
-if (Display_State == Display_Sensor_Test){
+
+  shakemotor();
+
+  if (Display_State == Display_Sensor_Test){
     Sensor_Test();
-}
-else{
-//*/
-  if (!(LeftSensor && RightSensor))
-  {
-      // digitalWrite(3, HIGH);
-/* from the man
-    // put your main code here, to run repeatedly:
-    if (counter == 100)
-    {
-        DriveMotor->setSpeed(255);
-    }
-    if (counter == 150)
+    return;
+  }
+  if (LeftSensor && RightSensor){
+      MFS.write("SENS");
+      Stop();
+      return;
+  }
 
-    {
-
-        DriveMotor->run(BACKWARD);
-    }
-    if (counter < 200)
- {
-   MFS.write((int)counter);
-   counter++;
- }
- else if (!ended)
- {
-   ended = true;
-   //MFS.write("End");
-   //MFS.blinkDisplay(DIGIT_ALL, ON);
-   // DriveMotor->run(RELEASE);
-   error();
- }
-
-//error();
- delay(50);
-
-}
-*/
-
-if (millis() < DisplayTime){
-    Time_Transition = true;
- switch (Display_State){
-     case Display_Min:
-     MFS.write("LO",1);
-     break;
-     case Display_ShakeMotor_Off:
-     MFS.write("OFF",1);
-     break;
-     case Display_ShakeMotor_On:
-     MFS.write("ON",1);
-     break;
-     case Display_Stop:
-     break;
-     case Display_MotorSpeed:
-     break;
-     case Display_Error:
-     break;
-     case Display_Sensor_Test:
-     break;
- }
-}
-else if (Time_Transition){
-        Time_Transition = false;
-        Display_State = Display_MotorSpeed;
-        MFS.write(DriveMotorCycleTime);
-}
+      if (millis() < DisplayTime){
+          Time_Transition = true;
+          switch (Display_State){
+            case Display_Min:
+            MFS.write("LO",1);
+            break;
+            case Display_ShakeMotor_Off:
+            MFS.write("OFF",1);
+            break;
+            case Display_ShakeMotor_On:
+            MFS.write("ON",1);
+            break;
+            case Display_Stop:
+            break;
+            case Display_MotorSpeed:
+            break;
+            case Display_Error:
+            break;
+            case Display_Sensor_Test:
+            break;
+          }
+        }
+        else if (Time_Transition){
+          Time_Transition = false;
+          Display_State = Display_MotorSpeed;
+          MFS.write(DriveMotorCycleTime);
+        }
 else{
         switch (Display_State){
             case Display_Min:
@@ -202,18 +116,6 @@ else{
             break;
         }
     }
-
-
-/*if((btn==BUTTON_1_PRESSED)&&(btn==BUTTON_3_PRESSED)){
-    Display_State = Display_Sensor_Test;
-*/
-//}
-/*else{
-    DriveMotor_State = DriveMotor_Error;
-}
-*/
-//else{
-
     if(btn==BUTTON_1_PRESSED){
         if (Display_State == Display_Stop){
             DriveMotorCycleTime = 10;
@@ -254,12 +156,6 @@ else{
             Display_State = Display_ShakeMotor_Off;
         }
     }
-
-
-
-
-//else
-//{
  switch(DriveMotor_State)
  {
   case DriveMotor_Stop:
@@ -320,58 +216,41 @@ else{
     DriveMotor->run(RELEASE);
   }
 
-//}
-//} //end double press else
-}
-else{
-    MFS.write("SENS");
-    Stop();
-    //Display_State = Display_Sensor_Test;
-}
-}
+
+
 }
 
 void Error() {
-/*
-      DriveMotor->setSpeed(255);
-
-      MFS.write("1111");
-      MFS.blinkDisplay(DIGIT_ALL, ON);
-      DriveMotor->run(BACKWARD);
-*/
-      //DriveMotor->setSpeed(0);
-      //ShakeMotor->setSpeed(0);
       Stop();
       DriveMotor_State = DriveMotor_Error;
       Display_State = Display_Error;
-      //MFS.write("ERR");
       MFS.blinkDisplay(DIGIT_ALL, ON);
 }
-
 void MovingLeft(bool LeftSensor, bool RightSensor){
-
 }
 void Stop(){
-  //MFS.write("STOP");
   Display_State = Display_Stop;
   DriveMotor_State = DriveMotor_Stop;
   ShakeMotorRunning = false;
   DriveMotor->run(RELEASE);
   ShakeMotor->run(RELEASE);
 }
+void shakemotor(){
+  if(ShakeMotorRunning){
+    ShakeMotor->run(FORWARD);
+  }
+  else{
+    ShakeMotor->run(RELEASE);
+  }
+}
 
 void MovingRight(bool LeftSensor, bool RightSensor){
-
 }
-
 
 void PausedLeft(bool LeftSensor,bool RightSensor){
-
 }
 
-
 void PausedRight(bool LeftSensor, bool RightSensor){
-
 }
 
 void Sensor_Test(){
@@ -387,5 +266,4 @@ void Sensor_Test(){
     else {
         MFS.write("NONE");
     }
-
 }
--- src/main.hpp
+++ src/main.hpp
@@ -4,3 +4,23 @@
 #include <Adafruit_MotorShield.h>
 #include <MultiFuncShield.h>
 #include <TimerOne.h>
+
+enum motor_type {
+    DriveMotor_Stop,
+    DriveMotor_MovingLeft,
+    DriveMotor_MovingRight,
+    DriveMotor_PausedLeft,
+    DriveMotor_PausedRight,
+    DriveMotor_Error,
+    DriveMotor_Engaged
+};
+motor_type DriveMotor_State = DriveMotor_Stop;
+enum display_type {
+    Display_MotorSpeed,
+    Display_Min,
+    Display_ShakeMotor_On,
+    Display_ShakeMotor_Off,
+    Display_Stop,
+    Display_Error,
+    Display_Sensor_Test
+};
